b8991a13a8a23dec915763faf3ae42249f532b97
LucasPettit <lukep>
2016-01-30 13:11:40 -0800

    Luke: added jugodatabase source file and a main jugo_db.java file

---
+++ Polarity_new/ParseStarterProject/src/main/java/com/parse/starter/jugodatabase/jugo_db.java
@@ -0,0 +1,11 @@
+package com.parse.starter.jugodatabase;
+
+/**
+ * Created by lpett on 1/30/2016.
+ * This is the main database file...
+ * I dont actually know if we'll need it
+ */
+public class jugo_db {
+
+
+}
