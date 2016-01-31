eea15947188d081c9b4687b61fc8c2c152103425
LucasPettit <lukep>
2016-01-30 19:47:54 -0800

    Luke starting to implament linking between pages. program still crashes when pressing buttons tho

--- Polarity_new/ParseStarterProject/src/main/java/com/parse/starter/HubActivity.java
+++ Polarity_new/ParseStarterProject/src/main/java/com/parse/starter/HubActivity.java
@@ -2,6 +2,8 @@ package com.parse.starter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
 
 public class HubActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class HubActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hub);
+    }
+
+    public void onLogoutClick(View view){
+
+        //TODO:
+        // Log out of user account
+
+
+        Intent intentObject = new Intent(this, MainActivity.class);
+        startActivity(intentObject);
     }
 }
