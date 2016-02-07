1335a19296aa499d129b4f2e8dccb1d31d28d97b
Cameron Ellis <cellis@ucsd.edu>
2016-02-06 16:34:26 -0800

    Cameron: Working on implementing the movie list

--- Polarity_new/ParseStarterProject/src/main/java/com/parse/starter/AddMovies.java
+++ Polarity_new/ParseStarterProject/src/main/java/com/parse/starter/AddMovies.java
@@ -4,13 +4,31 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ListView;
+import java.util.ArrayList;
+
 
 public class AddMovies extends AppCompatActivity {
+
+    ListView listOfMovies;
+    EditText movieToAdd;
+    ArrayList<Model> movieList;
+    CustomAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_movies);
+
+        listOfMovies = (ListView) findViewById(R.id.listOfMovies);
+        movieToAdd = (EditText) findViewById(R.id.movieToAdd);
+
+
+    }
+    // Add movie titles to the list
+    public void addMovieName(View view){
+
     }
 
     public void toActivityCreateEvent(View view){
---
+++ Polarity_new/ParseStarterProject/src/main/java/com/parse/starter/CustomAdapter.java
@@ -0,0 +1,7 @@
+package com.parse.starter;
+
+/**
+ * Created by Cameron on 2/6/2016.
+ */
+public class CustomAdapter {
+}
