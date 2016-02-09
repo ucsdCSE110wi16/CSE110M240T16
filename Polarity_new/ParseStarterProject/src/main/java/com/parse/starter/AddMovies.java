ccc54e96290950621e363e26ac379d66af3e9f9e
LucasPettit <lukep>
2016-02-09 11:42:45 -0800

    Cameron & Lucas: implemented list functionality for Add Movies

--- Polarity_new/ParseStarterProject/src/main/java/com/parse/starter/AddMovies.java
+++ Polarity_new/ParseStarterProject/src/main/java/com/parse/starter/AddMovies.java
@@ -2,9 +2,9 @@ package com.parse.starter;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -13,24 +13,75 @@ import java.util.ArrayList;
 
 public class AddMovies extends Activity {
 
-    ListView lv;
-    EditText et;
+    //region Declare Variables
+
+    Button btnBack, btnHome, btnImportMovieQueue, btnSearch;
+    ListView lvMovieList;
+    EditText tbSearch;
 
     ArrayList<Model> modelList;
     CustomAdapter adapter;
+
+    //endregion
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_movies);
 
-//        lv = (ListView) findViewById(R.id.searchImageView);
-        et = (EditText) findViewById(R.id.searchField);
+        // initialize variables
+        lvMovieList = (ListView) findViewById(R.id.addMovies_lvMovieList);
+        tbSearch = (EditText) findViewById(R.id.addMovies_tbSearch);
+        btnBack = (Button) findViewById(R.id.addMovies_btnBack);
+        btnHome = (Button) findViewById(R.id.addMovies_btnHome);
+        btnImportMovieQueue = (Button) findViewById(R.id.addMovies_ImportMovieQueue);
+        btnSearch = (Button) findViewById(R.id.addMovies_btnSearch);
 
         modelList = new ArrayList<Model>();
         adapter = new CustomAdapter(getApplicationContext(), modelList);
-        lv.setAdapter(adapter);
+        lvMovieList.setAdapter(adapter);
+
+
+        // set catches
+        btnSearch.setOnClickListener(btnSearch_Click());
+
     }
+
+    //region Button Click Events
+
+    protected View.OnClickListener btnSearch_Click() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addMovie(v);
+            }
+        };
+    } // btnSearch_Click
+
+    //endregion
+
+    //region Helper Methods
+
+    protected void addMovie(View v){
+        String name = tbSearch.getText().toString();
+        // if name field is empty, show toast notification
+        if (name.isEmpty()) {
+            Toast.makeText(getApplicationContext(), "You must enter a movie name into the text box",
+                    Toast.LENGTH_SHORT).show();
+        }
+        // add string to the list
+        else {
+            Model md = new Model(name);
+            modelList.add(md);
+            adapter.notifyDataSetChanged();
+            // reset input field
+            tbSearch.setText("");
+        }
+    }
+
+    //endregion
+
+    //region Navigation
 
     public void toActivityCreateEvent(View view){
         Intent intent = new Intent(this, CreateEvent.class);
@@ -42,20 +93,5 @@ public class AddMovies extends Activity {
         startActivity(intent);
     }
 
-    public void addbtn(View v){
-        String name = et.getText().toString();
-        // if name field is empty, show toast notification
-        if (name.isEmpty()) {
-            Toast.makeText(getApplicationContext(), "Plz enter Values",
-                    Toast.LENGTH_SHORT).show();
-        }
-        // add string to the list
-        else {
-            Model md = new Model(name);
-            modelList.add(md);
-            adapter.notifyDataSetChanged();
-            // reset input field
-            et.setText("");
-        }
-    }
+    //endregion
 }
