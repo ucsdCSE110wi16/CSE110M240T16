c1da14ba1357f36010b0fc9fddbb61b2c3e2a099
phtran <petertran.h@gmail.com>
2016-03-06 11:05:40 -0800

    Peter: I reworked this test file to utilize Robotium and test the capabilities of the LogIn activity

--- Polarity_new/ParseStarterProject/src/androidTest/java/com/parse/starter/LoginTest.java
+++ Polarity_new/ParseStarterProject/src/androidTest/java/com/parse/starter/LoginTest.java
@@ -1,36 +1,85 @@
 package com.parse.starter;
 
-import android.support.test.rule.ActivityTestRule;
-import android.support.test.runner.AndroidJUnit4;
+import com.parse.starter.LogIn;
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.EditText;
 
-import com.parse.ParseException;
-import com.parse.ParseUser;
-
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.action.ViewActions.typeText;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static com.parse.starter.R.id.login_tbUserName;
+import com.robotium.solo.Solo;
 
 /**
- * Created by PTRAN on 2/10/2016.
+ * Created by PTRAN on 3/5/2016.
  */
+public class LogInTest extends ActivityInstrumentationTestCase2<LogIn>{
+    private Solo solo;
 
-@RunWith(AndroidJUnit4.class)
-public class LoginTest {
-    @Rule
-    public ActivityTestRule<LogIn> loginActivityTestRule = new ActivityTestRule(LogIn.class);
+    public LogInTest(){
+        super(LogIn.class);
+    }
 
-    @Test
-    public void dummy_login(){
-        onView(withId(R.id.login_tbUserName)).perform(typeText("rustyshackleford"));
-        onView(withId(R.id.login_tbPassword)).perform(typeText("beer"));
+    @Override
+    protected void setUp() throws Exception{
+        super.setUp();
+        solo = new Solo(getInstrumentation(), getActivity());
+    }
 
-        onView(withId(R.id.login_btnLogin)).perform(click());
+    public void testAccessForgotPassword(){
+        solo.assertCurrentActivity("Are we on the login page?", LogIn.class);
+        solo.clickOnButton("Forgot Password");
+        solo.waitForActivity("com.parse.starter.ForgotPassword", 100);
+        solo.assertCurrentActivity("The activity should be the ForgotPassword", ForgotPassword.class);
+        solo.goBack();
+        solo.waitForActivity("com.parse.starter.LogIn", 100);
+        solo.assertCurrentActivity("The activity should be the LogIn", LogIn.class);
+    }
+
+    public void testAccessSignUp(){
+        solo.assertCurrentActivity("Are we on the login page?", LogIn.class);
+        solo.clickOnButton("Create Account");
+        solo.waitForActivity("com.parse.starter.SignUp", 100);
+        solo.assertCurrentActivity("The activity should be the SignUp", SignUp.class);
+        solo.goBack();
+        solo.waitForActivity("com.parse.starter.LogIn", 100);
+        solo.assertCurrentActivity("The activity should be the LogIn", LogIn.class);
+    }
+
+    public void testInvalidLogIn(){
+        solo.assertCurrentActivity("Are we on the login page?", LogIn.class);
+        solo.clickOnButton("Login");
+        solo.searchText("invalid login parameters");
+        solo.assertCurrentActivity("The activity should be the LogIn", LogIn.class);
+        solo.typeText((EditText) solo.getView(R.id.login_tbUserName), "phtran");
+        solo.typeText((EditText) solo.getView(R.id.login_tbPassword), "phtran");
+        solo.clickOnButton("Login");
+        solo.searchText("invalid login parameters");
+        solo.assertCurrentActivity("Again, the activity should be the LogIn", LogIn.class);
+        solo.clearEditText((EditText) solo.getView(R.id.login_tbUserName));
+        solo.clearEditText((EditText) solo.getView(R.id.login_tbPassword));
+        solo.typeText((EditText) solo.getView(R.id.login_tbUserName), "rxzero0079");
+        solo.typeText((EditText) solo.getView(R.id.login_tbPassword), "rxzero0079");
+        solo.clickOnButton("Login");
+        solo.searchText("invalid login parameters");
+        solo.assertCurrentActivity("Do you want me to repeat myself, it should be the LogIn", LogIn.class);
+        solo.clearEditText((EditText) solo.getView(R.id.login_tbUserName));
+        solo.clearEditText((EditText) solo.getView(R.id.login_tbPassword));
+        solo.typeText((EditText) solo.getView(R.id.login_tbUserName), "rxzero0079");
+        solo.typeText((EditText) solo.getView(R.id.login_tbPassword), "phtran");
+        solo.clickOnButton("Login");
+        solo.searchText("invalid login parameters");
+        solo.assertCurrentActivity("Get the hell out of here, you should be in the Login", LogIn.class);
+        solo.clearEditText((EditText) solo.getView(R.id.login_tbUserName));
+        solo.clearEditText((EditText) solo.getView(R.id.login_tbPassword));
+    }
+
+
+    public void testLogin(){
+        solo.assertCurrentActivity("Are we on the login page?", LogIn.class);
+        solo.typeText((EditText) solo.getView(R.id.login_tbUserName), "phtran");
+        solo.typeText((EditText) solo.getView(R.id.login_tbPassword), "rxzero0079");
+        solo.clickOnButton("Login");
+        solo.waitForActivity("com.parse.starter.HubActivity",100);
+        solo.assertCurrentActivity("The activity should be the HubActivity", HubActivity.class);
+        solo.clickOnButton("Log Out");
+        solo.waitForActivity("com.parse.starter.LogIn", 100);
+        solo.assertCurrentActivity("The activity should be the LogIn", LogIn.class);
     }
 }
